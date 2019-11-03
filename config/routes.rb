5b2dad1a9e5e1caa0c21f626bbb8f92515c32e4f
elijah johnston <eli@millinno.com>
2019-11-03 22:50:29 +0800

    testing code

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  devise_for :users
+  root 'teddies#index'
+
+  resources :teddies, only: [:index, :show]
+  resources :orders, only: [:show, :create]
+
+  resources :orders, only: [:show, :create] do
+    resources :payments, only: :new
+  end
+end
